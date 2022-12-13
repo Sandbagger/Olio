e899df382623ac724d6cf69857063a09288f4355
Will <wneal_19@yahoo.co.uk>
2022-12-13 15:43:04 +0100

    Added config for Capybara and added basic spec

---
+++ spec/features/artical_spec.rb
@@ -0,0 +1,10 @@
+require "rails_helper"
+
+RSpec.describe "Artical", :type => :feature do
+  scenario "User uploads a books csv" do
+    visit ""
+
+    expect(page).to have_content 'Bananas'
+    expect(page).to have_content 'Likes 1'
+  end
+end 
