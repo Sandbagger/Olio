a0e0a7ec0f259ae38a0469dd809ad9a4ab760604
Will <wneal_19@yahoo.co.uk>
2022-12-14 10:44:13 +0100

    Added navbar and updated spec

--- spec/features/artical_spec.rb
+++ spec/features/artical_spec.rb
@@ -1,9 +1,9 @@
 require "rails_helper"
 
 RSpec.describe "Artical", :type => :feature do
-  let(:url) {'https://s3-eu-west-1.amazonaws.com/olio-staging-images/developer/test-articles-v4.json'}
-  let(:body){ JSON.parse(File.read('spec/response.json').to_json, object_class: OpenStruct) }
-  let(:response) { instance_double(HTTParty::Response, body: body ) }
+  let(:url) { 'https://s3-eu-west-1.amazonaws.com/olio-staging-images/developer/test-articles-v4.json' }
+  let(:body) { JSON.parse(File.read('spec/response.json').to_json, object_class: OpenStruct) }
+  let(:response) { instance_double(HTTParty::Response, body: body) }
 
   before do
     allow(HTTParty).to receive(:get).with(url).and_return response
@@ -12,6 +12,7 @@ RSpec.describe "Artical", :type => :feature do
   scenario 'A user clicks like on an Artical' do
     visit ''
 
+    expect(page).to have_content 'Olio'
     expect(page).to have_content 'Ambipur air freshener plugin'
     expect(page.find('.card-img-top')['src']).to have_content 'https://cdn.olioex.com/uploads/photo/file/00gRGrBRDFYrR2j-9SJVYg/small_image.jpg'
     expect(page.find('.card-img-top')['alt']).to have_content 'Image of a Ambipur air freshener plugin'
